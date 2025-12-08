13dcc4d04df837236fc8794718479d86f63d80e2
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2025-12-08 17:43:41 +0900

    code review by gemini

--- server/main.py
+++ server/main.py
@@ -4,12 +4,11 @@ from typing import Literal, Optional, List
 
 app = FastAPI()
 
-database = []
+fake_db = []
 
 
 def find_todo(todo_id: int):
-    todo = next((todo for todo in database if todo["id"] == todo_id), None)
-    return todo
+    return next((t for t in fake_db if t["id"] == todo_id), None)
 
 
 class Todo(BaseModel):
@@ -34,44 +33,43 @@ class TodoUpdate(BaseModel):
     is_active: Optional[bool] = None
 
 
-@app.get("/todos", status_code=200, response_model=List[Todo])
+@app.get("/todos", response_model=List[Todo])
 def read_todos():
-    active_todo = [todo for todo in database if todo["is_active"]]
-    return active_todo
+    return [todo for todo in fake_db if todo["is_active"]]
 
 
 @app.post("/todos", status_code=201, response_model=Todo)
 def add_todo(todo: TodoCreate):
     new_todo = todo.model_dump()  # 把 pydantic class 轉 dicts
-    new_todo["id"] = len(database) if len(database) == 0 else database[-1]["id"] + 1
-    database.append(new_todo)
+    new_todo["id"] = len(fake_db) + 1 if len(fake_db) == 0 else fake_db[-1]["id"] + 1
+    fake_db.append(new_todo)
     return new_todo
 
 
 @app.get("/todos/{todo_id}", response_model=Todo)
-def get_todo_id(todo_id: int):
+def get_todo_by_id(todo_id: int):
     current_todo = find_todo(todo_id)
     if not current_todo:
-        raise HTTPException(status_code=404, detail="todo not found")
+        raise HTTPException(status_code=404, detail="Todo not found")
     return current_todo
 
 
 @app.patch("/todos/{todo_id}", response_model=Todo)
-def update_todo(todo_id: int, todo_update: TodoUpdate):
+def update_todo(todo_id: int, todo_payload: TodoUpdate):
     current_todo = find_todo(todo_id)
     if not current_todo:
-        raise HTTPException(status_code=404, detail="todo not found")
+        raise HTTPException(status_code=404, detail="Todo not found")
 
-    update_todo = todo_update.model_dump(exclude_unset=True)
-    current_todo.update(update_todo)
+    update_data = todo_payload.model_dump(exclude_unset=True)
+    current_todo.update(update_data)
 
     return current_todo
 
 
-@app.delete("/todos/{todo_id}")
+@app.delete("/todos/{todo_id}", status_code=204)
 def delete_todo(todo_id: int):
     current_todo = find_todo(todo_id)
     if not current_todo:
-        raise HTTPException(status_code=404, detail="todo not found")
+        raise HTTPException(status_code=404, detail="Todo not found")
     current_todo["is_active"] = False
-    return {"result": "success"}
+    return
